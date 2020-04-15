0ddd65a4501abd7bb265b29292b9c1cfc31f4fee
Bhavesh Kumar <2000bhaveshkumar@gmail.com>
2020-04-15 16:20:46 +0530

    Adding .env Environment variables

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+const encrypt = require('mongoose-encryption');
 
-const app =express();
+
+const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -16,8 +18,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-var secret = "Thisisourlittlesecret."
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
